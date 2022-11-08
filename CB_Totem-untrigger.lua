c51a656b39aa0e637e751a7ecd7a5aef732e1227
Daniel Rossa <danielrossa92@gmail.com>
2022-11-08 18:57:23 +0100

    initial commit

---
+++ CB_Totem-untrigger.lua
@@ -0,0 +1,17 @@
+function(event,timestamp,subevent,...)
+
+    if select(10,...) == 201764 then
+
+        aura_env["healingCount"] = nil;
+        aura_env["totemActive"] = false;
+        return true;
+
+    elseif aura_env["expirationTime"] <= GetTime() then
+
+        aura_env["healingCount"] = nil;
+        aura_env["totemActive"] = false;
+        return true;
+
+    end
+
+end
