2f34cd20a6ad4e4788801f952b49df0c4199f698
Daniel Rossa <danielrossa92@gmail.com>
2023-09-14 22:15:58 +0200

    changed: refactored code

--- duration.lua
+++ duration.lua
@@ -1,13 +1,9 @@
 function(...)
 
     if aura_env["expirationTime"] then
-
         local duration = 18;
         local expirationTime = aura_env["expirationTime"];
-
         return duration, expirationTime;
-
     end
-
     return false
 end
--- trigger.lua
+++ trigger.lua
@@ -1,31 +1,19 @@
 function(event,timestamp,subevent,...)
 
     if select(3,...) == GetUnitName("player") then
-
         if select(10,...) == 157153 then
-
             aura_env["expirationTime"] = GetTime() + 18;
             aura_env["healingCount"] = 0;
             aura_env["totemActive"] = true;
-
             return true;
 
         elseif subevent == "SPELL_HEAL" or subevent == "SPELL_PERIODIC_HEAL" and aura_env["totemActive"] then
-
             if select(10,...) == 204266 then
-
                 return false;
-
             end
-
             aura_env["healingCount"] = aura_env["healingCount"] + select(13,...) * 0.3;
-
             return true;
-
         end
-
     end
-
     return false;
-
 end
