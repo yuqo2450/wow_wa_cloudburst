2b998e0582c6f6f116feca1d70c230942def2ddc
Daniel Rossa <79540477+yuqo2450@users.noreply.github.com>
2023-11-04 21:17:02 +0100

    Added support for WoW version 10.1.7 (#2)
    * changed: increase duration to 18 seconds
    
    * changed: file structure
    
    * changed: refactored code
    
    * added: icon code
    
    * added: retun value for unused subevents
    
    * changed: new import string added
    
    * changed: file name for import string was changed to be more general
    
    * added: github actions workflow to automatically create new release when new tag is published
    
    * fixed: typo
    
    * changed: updated code to work in df version 10.1.7
    
    * changed: table keys are accessed with . notation
    
    * changed: updated import string
    
    * changed: updated release workflow
    
    * changed: updated to maintained action
    
    * removed: dispensable subevents
    
    * added: changelog
    
    * changed: set correct date for older version
    
    * changed: updated docs

---
+++ duration.lua
@@ -0,0 +1,6 @@
+function(...)
+  if aura_env.expirationTime then
+    return aura_env.totemDuration, aura_env.expirationTime;
+  end
+  return false
+end
---
+++ untrigger.lua
@@ -0,0 +1,30 @@
+function(event,arg1,arg2,...)
+  if event == "PLAYER_TOTEM_UPDATE" then
+    local totemActive = GetTotemInfo(arg1);
+    if not totemActive and arg1 == aura_env.totemIndex then
+      aura_env.totemActive = nil;
+      aura_env.totemName = nil;
+      aura_env.totemDuration = nil;
+      aura_env.totemIndex = nil;
+      aura_env.expirationTime = nil;
+      aura_env.healingCount = nil;
+      return true;
+    end
+  end
+
+  if arg2 == "SPELL_CAST_SUCCESS" then
+    local unit = select(3,...);
+    local spell = select(10,...);
+
+    if unit == GetUnitName("player").."-"..GetRealmName() and spell == 201764 then
+      aura_env.totemActive = nil;
+      aura_env.totemName = nil;
+      aura_env.totemDuration = nil;
+      aura_env.totemIndex = nil;
+      aura_env.expirationTime = nil;
+      aura_env.healingCount = nil;
+      return true;
+    end
+  end
+  return false;
+end
