ccbe04b4221919f505d93617a56d20beb89411a2
Daniel Rossa <danielrossa92@gmail.com>
2024-05-29 15:43:24 +0200

    fix(ligic): use reverse logic

--- trigger.lua
+++ trigger.lua
@@ -2,19 +2,20 @@
 function(event,arg1,arg2,...)
   if event == "PLAYER_TOTEM_UPDATE" then
     aura_env.totemActive, aura_env.totemName = GetTotemInfo(arg1);
-    if aura_env.totemActive and aura_env.totemName == "Cloudburst Totem" then
-      _,_,_, aura_env.totemDuration = GetTotemInfo(arg1);
-      aura_env.expirationTime = GetTime() + aura_env.totemDuration;
-      aura_env.totemIndex = arg1;
-      aura_env.healingCount = 0;
-      return true;
+    if not aura_env.totemActive or aura_env.totemName ~= "Cloudburst Totem" then
+      return false
     end
-
+    _,_,_, aura_env.totemDuration = GetTotemInfo(arg1);
+    aura_env.expirationTime = GetTime() + aura_env.totemDuration;
+    aura_env.totemIndex = arg1;
+    aura_env.healingCount = 0;
+    return true;
   elseif arg2 == "SPELL_HEAL" or arg2 == "SPELL_PERIODIC_HEAL" then
-    if select(2,...) == UnitGUID("player") and aura_env.totemActive then
-      aura_env.healingCount = aura_env.healingCount + select(13,...) * aura_env.HEALING_PERCENT;
-      return true;
+    if select(2,...) ~= UnitGUID("player") or not aura_env.totemActive then
+      return false
     end
+    aura_env.healingCount = aura_env.healingCount + select(13,...) * aura_env.HEALING_PERCENT;
+    return true;
   end
   return false;
 end
