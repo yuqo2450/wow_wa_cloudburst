0f823fb12648564d1fbf35afc71a3beabb42d3ad
Daniel Rossa <danielrossa92@gmail.com>
2024-05-29 15:33:04 +0200

    fix(logic): removed unnessescary code

--- untrigger.lua
+++ untrigger.lua
@@ -1,30 +1,19 @@
 function(event,arg1,arg2,...)
   if event == "PLAYER_TOTEM_UPDATE" then
-    local totemActive = GetTotemInfo(arg1);
-    if not totemActive and arg1 == aura_env.totemIndex then
-      aura_env.totemActive = nil;
-      aura_env.totemName = nil;
-      aura_env.totemDuration = nil;
-      aura_env.totemIndex = nil;
-      aura_env.expirationTime = nil;
-      aura_env.healingCount = nil;
-      return true;
+    if arg1 ~=  aura_env.totemIndex then
+      return false
     end
+    if GetTotemInfo(arg1) then
+      return false;
+    end
+    return true
   end
 
   if arg2 == "SPELL_CAST_SUCCESS" then
-    local guidCaster = select(2,...);
-    local spell = select(10,...);
-
-    if guidCaster == UnitGUID("player") and spell == 201764 then
-      aura_env.totemActive = nil;
-      aura_env.totemName = nil;
-      aura_env.totemDuration = nil;
-      aura_env.totemIndex = nil;
-      aura_env.expirationTime = nil;
-      aura_env.healingCount = nil;
-      return true;
+    if select(2,...) ~= UnitGUID("player") or select(10,...) ~= 201764 then
+      return false
     end
+    return true
   end
   return false;
 end
